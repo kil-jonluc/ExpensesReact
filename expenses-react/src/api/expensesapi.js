43346dc43b0a268ac899589c5514f036dcbda192
Jonathan Kilpatrick <kilpatrick.jonluc@outlook.com>
2020-03-26 21:13:20 -0400

    updated api exports

--- expenses-react/src/api/expensesapi.js
+++ expenses-react/src/api/expensesapi.js
@@ -4,6 +4,14 @@ export function getExpenses() {
   return axios.get("http://localhost:3001/expenses");
 }
 
-// export function addExpenses() {
-//   return axios.post("http://localhost:3001/expenses", expense);
-// }
+export function removeExpenses(id) {
+  return axios.delete(`http://localhost:3001/expenses/${id}`);
+}
+
+export function saveExpenses(expense) {
+  let url = "http://localhost:3001/expenses/";
+  return axios[expense.id ? "put" : "post"](url, expense);
+}
+
+// TODO
+// 1. Add Async/await
